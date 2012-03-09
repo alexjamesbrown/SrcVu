bbd01fff8f92cbbd1798e0836b0ca6c3480b81f1
Alex Brown <alex@alexjamesbrown.com>
2012-03-09 11:55:28 +0000

    shorted out favicon requests

--- app.js
+++ app.js
@@ -3,6 +3,13 @@ var http = require('http'),
 
 var server = http.createServer(function (req, resp) {
 	
+	// control for favicon
+	  if (q.url === '/favicon.ico') {
+	    r.writeHead(200, {'Content-Type': 'image/x-icon'} );
+	    r.end();
+	    return;
+	  }
+	
 	var url = req.url.slice(1);
 
 	request(url, function (error, response, body) {
