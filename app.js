e6237b9cf7210cde705ef42d212058949aaf3fdc
Alex Brown <alex@alexjamesbrown.com>
2012-03-09 11:57:37 +0000

    changed around favicon url handling

--- app.js
+++ app.js
@@ -3,15 +3,15 @@ var http = require('http'),
 
 var server = http.createServer(function (req, resp) {
 	
+	var url = req.url.slice(1);
+	
 	// control for favicon
-	  if (q.url === '/favicon.ico') {
+	  if (url === 'favicon.ico') {
 	    r.writeHead(200, {'Content-Type': 'image/x-icon'} );
 	    r.end();
 	    return;
 	  }
 	
-	var url = req.url.slice(1);
-
 	request(url, function (error, response, body) {
 		if (!error && response.statusCode == 200) {
 			resp.writeHeader(200, {"Content-Type": "text/plain"}); 
