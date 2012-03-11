d4401af5ce4b8a1e45f63eb8838c881ee6efb63a
Alex Brown <alex@alexjamesbrown.com>
2012-03-11 04:13:36 +0000

    fixes #2 - added js prettify - works well. also added index view. will expand on this later

--- app.js
+++ app.js
@@ -1,34 +1,38 @@
 var request = require('request'),
-	express = require('express');
+express = require('express');
 
-var app = express.createServer(
-	express.favicon()
+var app = express.createServer();
+
+//config
+var port = process.env.PORT || 8080;
+
+app.configure(function(){
+	app.set("view options", { layout: false, pretty: true });
+	app.use(express.favicon());
+	app.use(express.static(__dirname + '/public'));
+}
 );
 
+app.listen(port);
+
+// Routes
 app.get('/', function(req, resp){
-	resp.writeHeader(200, {"Content-Type": "text/plain"}); 
-	resp.write("no url set");
-	resp.end();
+	resp.render('index.jade', {pageTitle: 'Some title'});
 });
 
 app.get('/:url', function(req, resp){
-	
 	var url = req.params.url;
 
 	//ensure url starts with http:// (or we get an invalid protocol exception from request)
 	if (url.substring(0, 7) != "http://") {
-	    url="http://"+url;
+		url="http://"+url;
 	}
-		
+
 	request(url, function (error, response, body) {
-		if (!error && response.statusCode == 200) {
-			resp.writeHeader(200, {"Content-Type": "text/plain"}); 
-			resp.write(body);
-			resp.end();
+		if (!error && response.statusCode == 200) {			
+			resp.render('src.jade', {pageTitle: 'Source of: '+url, content: body})
 		}
 	})
 });
 
-var port = process.env.PORT || 8080;
-//console.log(port);
 app.listen(port);
