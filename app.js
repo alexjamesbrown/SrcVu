c2a2e338b5592640f194fdce7d91fa8473b6670b
Alex Brown <alex@alexjamesbrown.com>
2015-05-25 22:40:29 +0100

    moved to ejs removed module - put in route for now since it's so small general tidy up

--- app.js
+++ app.js
@@ -1,29 +1,51 @@
 var request = require('request'),
-	express = require('express'),
-	getSource = require('./modules/getsource'),
-	app = express.createServer(),
-	port = process.env.PORT || 5000;
+    express = require('express'),
+    app = express.createServer(),
+    port = process.env.PORT || 5000;
 
-app.configure(function(){
-	app.set('view options', { layout: false, pretty: true });
-	app.set('view engine', 'ejs')
-	app.use(express.bodyParser());
-	app.use(express.favicon());
-	app.use(express.static(__dirname + '/public'));
+app.configure(function() {
+    app.set('view options', {
+        layout: false,
+        pretty: true
+    });
+    app.set('view engine', 'ejs')
+    app.use(express.bodyParser());
+    app.use(express.favicon());
+    app.use(express.static(__dirname + '/public'));
 });
 
-// Routes
-app.get('/', function(req, resp){
-	resp.render('index.ejs');
+app.get('/', function(req, resp) {
+    resp.render('index.ejs');
 });
 
-app.get('/:url', function(req, resp){
-	getSource(req.params.url, resp);
+app.get('/:url', function(req, res) {
+
+    var url = req.param('url');
+
+    //ensure url starts with http:// (or we get an invalid protocol exception from request)
+    if (url.substring(0, 7) != "http://") {
+        url = "http://" + url;
+    }
+
+    request(url, function(err, response, body) {
+        if (err) {
+            return res.render('error.ejs', {
+                error: error
+            });
+        }
+
+        if (response.statusCode == 200) {
+            return res.render('src.ejs', {
+                url: url,
+                content: body
+            })
+        }
+    })
 });
 
-app.post('/', function(req, resp){
-	resp.redirect('/' + req.param('url', null));
+app.post('/', function(req, resp) {
+    resp.redirect('/' + req.param('url', null));
 });
 
 app.listen(port);
-console.log('Listening on port '+port);
+console.log('Listening on port ' + port);
